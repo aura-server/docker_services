068ffc37af6daadd51ba5a661bed7eae9c98c385
tianlong <tianlong@momenta.ai>
2018-11-22 04:27:45 +0000

    Add install_docker

---
+++ scripts/install_docker-ubuntu_amd64.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+## Code write on 2018/11/22
+set -xe
+
+
+## Install Docker
+sudo apt update
+sudo apt install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+
+## Install Docker-Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
