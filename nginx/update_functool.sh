84dc2f50e77142905db8a672cba68f9dcedae996
auroraka <yangtianlong111@gmail.com>
2020-02-26 14:37:22 +0000

    [feature] Remove functool

--- nginx/update_functool.sh
+++ nginx/update_functool.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
-git submodule update --recursive --init functool
+git submodule update --recursive --init --remote functool
 cp functool/functool.env data/functool/
 docker-compose down
 docker-compose up -d
